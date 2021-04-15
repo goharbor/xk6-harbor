d621cbb54011db50f154ad44020400f669c7973d
He Weiwei <hweiwei@vmware.com>
2021-04-15 09:01:32 +0000

    feat: add listUsers method for the harbor module
    Signed-off-by: He Weiwei <hweiwei@vmware.com>

--- pkg/module/user.go
+++ pkg/module/user.go
@@ -4,8 +4,10 @@ import (
 	"context"
 	"fmt"
 
+	"github.com/dop251/goja"
 	operation "github.com/heww/xk6-harbor/pkg/harbor/client/user"
 	"github.com/heww/xk6-harbor/pkg/harbor/models"
+	"github.com/loadimpact/k6/js/common"
 )
 
 func (h *Harbor) CreateUser(ctx context.Context, username string, passwords ...string) int64 {
@@ -28,4 +30,30 @@ func (h *Harbor) CreateUser(ctx context.Context, username string, passwords ...s
 	Checkf(ctx, err, "failed to create user %s", username)
 
 	return IDFromLocation(ctx, res.Location)
+}
+
+type ListUsersResult struct {
+	Projects []*models.UserResp `js:"users"`
+	Total    int64              `js:"total"`
+}
+
+func (h *Harbor) ListUsers(ctx context.Context, args ...goja.Value) ListUsersResult {
+	h.mustInitialized(ctx)
+
+	params := operation.NewListUsersParams()
+
+	if len(args) > 0 {
+		rt := common.GetRuntime(ctx)
+		if err := rt.ExportTo(args[0], params); err != nil {
+			common.Throw(common.GetRuntime(ctx), err)
+		}
+	}
+
+	res, err := h.api.User.ListUsers(ctx, params)
+	Checkf(ctx, err, "failed to list users")
+
+	return ListUsersResult{
+		Projects: res.Payload,
+		Total:    res.XTotalCount,
+	}
 }
