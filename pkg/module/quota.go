922f2546889ba8d4bfa61228df1cd1188e580ecc
He Weiwei <hweiwei@vmware.com>
2021-04-16 09:58:55 +0000

    feat: add listQuotas method to the module
    Signed-off-by: He Weiwei <hweiwei@vmware.com>

---
+++ pkg/module/quota.go
@@ -0,0 +1,36 @@
+package module
+
+import (
+	"context"
+
+	"github.com/dop251/goja"
+	operation "github.com/heww/xk6-harbor/pkg/harbor/client/quota"
+	"github.com/heww/xk6-harbor/pkg/harbor/models"
+	"github.com/loadimpact/k6/js/common"
+)
+
+type ListQuotasResult struct {
+	Quotas []*models.Quota `js:"quotas"`
+	Total  int64           `js:"total"`
+}
+
+func (h *Harbor) ListQuotas(ctx context.Context, args ...goja.Value) ListQuotasResult {
+	h.mustInitialized(ctx)
+
+	params := operation.NewListQuotasParams()
+
+	if len(args) > 0 {
+		rt := common.GetRuntime(ctx)
+		if err := rt.ExportTo(args[0], params); err != nil {
+			common.Throw(common.GetRuntime(ctx), err)
+		}
+	}
+
+	res, err := h.api.Quota.ListQuotas(ctx, params)
+	Checkf(ctx, err, "failed to list quotas")
+
+	return ListQuotasResult{
+		Quotas: res.Payload,
+		Total:  res.XTotalCount,
+	}
+}
