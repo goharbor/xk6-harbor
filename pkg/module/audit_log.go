e8b96bd17ea936910efd22acb69df974ba9f70e5
He Weiwei <hweiwei@vmware.com>
2021-04-16 09:46:37 +0000

    feat: add listAuditLogs method to the module
    Signed-off-by: He Weiwei <hweiwei@vmware.com>

---
+++ pkg/module/audit_log.go
@@ -0,0 +1,36 @@
+package module
+
+import (
+	"context"
+
+	"github.com/dop251/goja"
+	operation "github.com/heww/xk6-harbor/pkg/harbor/client/auditlog"
+	"github.com/heww/xk6-harbor/pkg/harbor/models"
+	"github.com/loadimpact/k6/js/common"
+)
+
+type ListAuditLogsResult struct {
+	AuditLogs []*models.AuditLog `js:"logs"`
+	Total     int64              `js:"total"`
+}
+
+func (h *Harbor) ListAuditLogs(ctx context.Context, args ...goja.Value) ListAuditLogsResult {
+	h.mustInitialized(ctx)
+
+	params := operation.NewListAuditLogsParams()
+
+	if len(args) > 0 {
+		rt := common.GetRuntime(ctx)
+		if err := rt.ExportTo(args[0], params); err != nil {
+			common.Throw(common.GetRuntime(ctx), err)
+		}
+	}
+
+	res, err := h.api.Auditlog.ListAuditLogs(ctx, params)
+	Checkf(ctx, err, "failed to list audit logs")
+
+	return ListAuditLogsResult{
+		AuditLogs: res.Payload,
+		Total:     res.XTotalCount,
+	}
+}
